2dab6f40586e13bdb67ce02a6d6056d0fd43b541
Hamid eng <hamiddikhan@gmail.com>
2023-09-21 00:42:32 -0700

    Add project files.

---
+++ Book.Store/Controllers/CateogoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Store.Controllers
+{
+    public class CateogoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
