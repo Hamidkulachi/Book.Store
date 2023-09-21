1b5d4914cc4c95f1f175651f4b682a6caed7f14d
Hamid eng <hamiddikhan@gmail.com>
2023-09-21 05:28:46 -0700

    product added to database and seed its data

---
+++ Book.Store.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Book.Store.Data;
+using Book.Store.DataAccess.Repository.IRepository;
+using Book.Store.Models;
+using Book.Store.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Store.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
