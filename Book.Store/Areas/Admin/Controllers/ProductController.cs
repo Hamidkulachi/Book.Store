7dc2981fd6eb86c01e7241ede27354eecc06c592
Hamid eng <hamiddikhan@gmail.com>
2023-09-23 07:38:08 -0700

    Navigation propertity

--- Book.Store/Areas/Admin/Controllers/ProductController.cs
+++ Book.Store/Areas/Admin/Controllers/ProductController.cs
@@ -19,8 +19,9 @@ namespace Book.Store.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> products = _unitOfWork.Product.GetAll().ToList();
-            return View(products);
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+
+            return View(objProductList);
         }
 
         public IActionResult UpSert(int? id)
