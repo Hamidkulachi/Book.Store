3830d629eda86cc4ddb62be6789daf39670d6d69
Hamid eng <hamiddikhan@gmail.com>
2023-09-26 23:16:34 -0700

    shoping cart added and its crud operation

--- Book.Store.Models/ShopingCart.cs
+++ Book.Store.Models/ShopingCart.cs
@@ -24,5 +24,8 @@ namespace Book.Store.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
