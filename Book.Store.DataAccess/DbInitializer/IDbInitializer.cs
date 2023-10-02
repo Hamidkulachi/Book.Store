295a3096134dd6b8985a032cb6f756893c19d3bc
Hamid eng <hamiddikhan@gmail.com>
2023-10-01 23:32:20 -0700

    db initializer

--- Book.Store.DataAccess/DbInitializer/IDbInitializer.cs
+++ Book.Store.DataAccess/DbInitializer/IDbInitializer.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Book.Store.DataAccess.DbInitializer
 {
-    internal class IDbInitializer
+    public interface IDbInitializer
     {
+        void Initialize();
     }
 }
